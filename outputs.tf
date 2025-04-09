76ccfa3208e935dafcc5a73d937e1bfbf269ec06
Agustin S <agustins@root-view.com>
2025-04-09 17:45:26 -0300

    iam_role_arn output added

--- outputs.tf
+++ outputs.tf
@@ -16,3 +16,8 @@ output "s3_bucket_region" {
   value       = aws_s3_bucket.hugo_website.region
   sensitive   = false
 }
+
+#IAM
+output "iam_role_arn" {
+  value = aws_iam_role.github_actions_role.arn
+}
